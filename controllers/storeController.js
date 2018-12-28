ecefb08fc86865ec83148bf5bac4b73f405877d4
Alex Perez <aperezm85@gmail.com>
2018-12-28 17:21:44 +0000

    Added pagination

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -55,9 +55,27 @@ exports.createStore = async (req, res) => {
 };
 
 exports.getStores = async (req, res) => {
+  const page = req.params.page || 1;
+  const limit = 4;
+  const skip = limit * page - limit;
   // get all stores
-  const stores = await Store.find();
-  res.render("stores", { title: "Stores", stores });
+  const storesPromise = Store.find()
+    .skip(skip)
+    .limit(limit)
+    .sort({ created: "desc" });
+
+  const countPromise = Store.count();
+  const [stores, count] = await Promise.all([storesPromise, countPromise]);
+  const pages = Math.ceil(count / limit);
+  if (!stores.length && skip) {
+    req.flash(
+      "info",
+      `Hey! You asked for page ${page}. But that doesn't exist. So I put you on the page ${pages}`
+    );
+    res.redirect(`/stores/page/${pages}`);
+    return;
+  }
+  res.render("stores", { title: "Stores", stores, count, pages, page });
 };
 
 const confirmOwner = (store, user) => {
--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,7 @@ router.post(
 );
 
 router.get("/stores", catchErrors(storeController.getStores));
+router.get("/stores/page/:page", catchErrors(storeController.getStores));
 router.get("/stores/:id/edit", catchErrors(storeController.editStore));
 // router.get('/reverse/:name', (req, res) => {
 //   const reverse = [...req.params.name].reverse().join('')
